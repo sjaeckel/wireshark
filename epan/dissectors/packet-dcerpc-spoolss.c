sjaeckel/wireshark

commit aa0141150c398e6001023fb255ba4dd8e349a507

tvb_length_remaining() can return -1, so be sure to deal with it if it does.
git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@43703 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-dcerpc-spoolss.c
@@ -1098,6 +1098,9 @@ dissect_spoolss_uint16uni(tvbuff_t *tvb, int offset, packet_info *pinfo _U_,
 	/* Get remaining data in buffer as a string */
 
 	remaining = tvb_length_remaining(tvb, offset);
+	if (remaining <= 0)
+		return offset;
+
 	text = tvb_get_unicode_string(tvb, offset, remaining, ENC_LITTLE_ENDIAN);
 	len = (int)strlen(text);
 
@@ -4183,7 +4186,7 @@ dissect_FORM_1(tvbuff_t *tvb, int offset, packet_info *pinfo,
 	/* Eek - we need to know whether this pointer was NULL or not.
 	   Currently there is not any way to do this. */
 
-	if (tvb_length_remaining(tvb, offset) == 0)
+	if (tvb_length_remaining(tvb, offset) <= 0)
 		goto done;
 
 	offset = dissect_ndr_uint32(

commit 4da136d288a89eb24fd5a866a9058403c42b5586

Fix https://bugs.wireshark.org/bugzilla/show_bug.cgi?id=7479 : Provide an empty string when data pointer is not NULL (regression introduced in r43703)
git-svn-id: http://anonsvn.wireshark.org/wireshark/trunk@43724 f5534014-38df-0310-8fa8-9805f1628bb7

epan/dissectors/packet-dcerpc-spoolss.c
@@ -1098,8 +1098,11 @@ dissect_spoolss_uint16uni(tvbuff_t *tvb, int offset, packet_info *pinfo _U_,
 	/* Get remaining data in buffer as a string */
 
 	remaining = tvb_length_remaining(tvb, offset);
-	if (remaining <= 0)
+	if (remaining <= 0) {
+		if (data)
+			*data = g_strdup("");
 		return offset;
+	}
 
 	text = tvb_get_unicode_string(tvb, offset, remaining, ENC_LITTLE_ENDIAN);
 	len = (int)strlen(text);